ca193e77852593e3aedca22ff1472198ce33a477
Gloridust <Gloridust@gmail.com>
2024-09-18 19:20:39 +0800

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,7 +16,7 @@ const About = () => {
             inSearch 是一个旨在为无法访问 Google 的用户提供纯净搜索体验的搜索引擎。我们采用简约、扁平化的设计，确保用户在搜索时拥有高效且愉悦的体验。
           </p>
           <p className="text-gray-700 dark:text-gray-300 mb-4">
-            后台依赖于 Google 的接口，确保搜索结果的准确性和全面性。inSearch 专注于提供最相关的网页搜索结果，同时保持界面的简洁和响应速度。
+            inSearch 专注于提供最相关的网页搜索结果，同时保持界面的简洁和响应速度。
           </p>
           <p className="text-gray-700 dark:text-gray-300 mb-4">
             感谢您使用 inSearch，希望我们的服务能够满足您的需求。
