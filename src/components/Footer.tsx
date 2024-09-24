7d3b1ae1d4ccafd00eea615c7ff3f41cbb40cbf3
Gloridust <Gloridust@gmail.com>
2024-09-25 00:19:10 +0800

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+// components/Footer.tsx
+
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-4 text-center text-sm text-gray-600 dark:text-gray-400">
+      <p>
+        © {new Date().getFullYear()} inSearch. 
+        <Link href="https://github.com/Gloridust/in-search" className="ml-1 hover:text-blue-500 transition-colors">
+          GitHub
+        </Link>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
